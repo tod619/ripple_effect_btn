c263b6966e4b22d2b8b17d23c35f6116da17e6cf
tod619 <trevorodwyer619@hotmail.com>
2023-02-06 15:03:21 +0000

    Create ripple with top + left mouse co-ords in scrip.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.btn')
+
+let ripple
+
+btn.addEventListener('mouseenter', (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left
+    const top = e.clientY - e.target.getBoundingClientRect().top
+    
+    ripple = document.createElement('div')
+    ripple.style.left = `${left}px`
+    ripple.style.top = `${top}px` 
+    btn.prepend(ripple)
+})
